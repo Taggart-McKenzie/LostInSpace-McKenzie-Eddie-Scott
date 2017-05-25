cddcf48b20de6b42a32d332875adac1454a0d697
sshipp57 <sshipp57@Illumiscotty>
2017-05-24 20:22:13 -0600

    5-24-2017 AlienControl

--- src/byui/cit260/moonJumpers/control/AlienControl.java
+++ src/byui/cit260/moonJumpers/control/AlienControl.java
@@ -11,4 +11,39 @@ package byui.cit260.moonJumpers.control;
  */
 public class AlienControl {
     
+    public static double calcRunningAway (double finalvelocity, double initialvelocity, double time){
+    
+        if (finalvelocity <= 0) {
+                return -1;
+                        }
+        if (initialvelocity <= 0 || initialvelocity > 15) {
+                return -1;
+                        }
+        if (time <= 0) {
+                return -1;
+        }
+        
+        
+         double acceleration = (finalvelocity - initialvelocity) / time;
+         
+         
+         return acceleration;
+    }
+    
+    public static double calcFightingMonster (double mass, double velocity, double time){
+        
+        if (mass <= 0) {
+            return -1;
+                    }
+        if (velocity <= 0 || velocity > 15){
+            return -1;
+                    }
+        if (time <=0) {
+            return -1;
+                    }
+        
+        double force = (mass * velocity) / time;
+        
+        return force;
+    }
 }
