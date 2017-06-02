5dc7d99a784c793aa7333298cd87091e56982ca3
sshipp57 <sshipp57@Illumiscotty>
2017-06-01 20:43:28 -0600

    6-1-2017 GameControl updated

--- src/byui/cit260/moonJumpers/control/GameControl.java
+++ src/byui/cit260/moonJumpers/control/GameControl.java
@@ -5,11 +5,18 @@
  */
 package byui.cit260.moonJumpers.control;
 
+import byui.cit260.moonJumpers.model.Player;
+
 /**
  *
  * @author Eddie Pincay
  */
 public class GameControl {
+
+    public static Player createPlayer(String playersName) {
+        System.out.println("\n*** createPlayer() function called ***");
+        return new Player();
+    }
     public void startNewGame(){
         
     }
