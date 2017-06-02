e2f45f6551df4ef80fe73ef63a0816276ad9487a
sshipp57 <sshipp57@Illumiscotty>
2017-06-01 19:57:01 -0600

    6-01-2017 startprogramview and banner

---
+++ src/byui/cit260/moonJumpers/view/StartProgramView.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.moonJumpers.view;
+
+/**
+ *
+ * @author sshipp57
+ */
+public class StartProgramView {
+    
+    private String promptMessage;
+    
+    public StartProgramView() {
+    
+        this.promptMessage = "\nPlease enter your name:";
+        // display the banner when view is created
+        this.displayBanner();
+    
+    }
+
+    private void displayBanner() {
+    
+        System.out.println(
+                "\n******************************************************************"
+               +"\n* Lost in Space is a text-based role-playing game where the      *"
+               +"\n* player of the game has found himself lost in space and         *"
+               +"\n* must use the clues and resources provided to him as he         *"
+               +"\n* navigates his way back to Earth safely. When he first awakens, *"
+               +"\n* he finds himself on Neptune, the eighth and farthest           *"
+               +"\n* known planet from the Sun in the Solar System. His journey     *"
+               +"\n* home may include visits to Uranus, Saturn, Jupiter, Mars,      *"
+               +"\n* Titan, or the Moon. Each location offers more clues and        *"
+               +"\n* that will help him earn fuel for his spaceship acquire weapons *"
+               +"\n* to fight off aliens, and ultimately find his way back to Earth *"
+               +"\n*                                                                *"
+               +"\n* The player awakens to find himself on an unfamiliar and barren *"
+               +"\n* planet, with intense winds and no sign of life. As he looks up *"
+               +"\n* to the night sky, he observes 14 distant moons, some larger    *"
+               +"\n* than others. His spaceship has crashed and he has lost all     *"
+               +"\n* communication with Earth. As he looks out the window, he notices*"
+               +"\n* a shiny metal box next to the spaceship. With his spacesuit    *"
+               +"\n* still intact, he opens the door of the spaceship and retrieves *"
+               +"\n* the metal box. Secured safely within the box is a map which    *"
+               +"\n* provides him with his first clue as he begins his journey back *"
+               +"\n* to Earth.                                                      *"
+               +"\n*                                                                *"
+               +"\n* Knowing he is a long way from home, the player desperately     *"
+               +"\n* wants to make it back to Earth safely. The goal of the game is *"
+               +"\n* for the player to return to Earth safely by navigating his way *"
+               +"\n* through space, fighting off dangerous alien creatures, and     *"
+               +"\n* collecting helpful clues and resources as he stops at each of  *"
+               +"\n* than others. His spaceship has crashed and he has lost all     *"
+               +"\n* the destinations along the way. The player will need to find the*"
+               +"\n* resources that will help him restore the spaceship, gather the *"
+               +"\n* necessary fuel, acquire weapons to fight aliens, and eventually*"
+               +"\n* land the spaceship safely back on Earth.                       *"
+               +"\n*                                                                *"
+               +"\n******************************************************************"
+        );
+    }
+    
+}
