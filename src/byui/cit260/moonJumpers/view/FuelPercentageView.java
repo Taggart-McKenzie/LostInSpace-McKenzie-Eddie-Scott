897cf84cb45299ba9a780f7b19888a05ef896b81
Eddie Pincay <eddiepincay@gmail.com>
2017-06-07 23:51:44 -0500

    06-07-2017 FuelPercentageView

--- src/byui/cit260/moonJumpers/view/FuelPercentageView.java
+++ src/byui/cit260/moonJumpers/view/FuelPercentageView.java
@@ -5,10 +5,73 @@
  */
 package byui.cit260.moonJumpers.view;
 
+import byui.cit260.moonJumpers.control.TravelControl;
+import java.util.Scanner;
+
 /**
  *
  * @author Eddie Pincay
  */
 public class FuelPercentageView {
+    private String fuelPercentage;
+    
+    public FuelPercentageView() {
+        this.fuelPercentage = "\nWhat is your Fuel            ";
+    }
+    
+    void displayFuelPercentageView() {
+
+        boolean done = false;
+        do {
+
+            String fuelPercentageOption = this.getFuelPercentageOption();
+            if (fuelPercentageOption.toUpperCase().equals("Q")) {
+                return;
+            }
+
+            done = this.doAction(fuelPercentageOption);
+        } while (!done);
+
+    }
+
+    private String getFuelPercentageOption() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println("\n" + this.fuelPercentage);
+
+            value = keyboard.nextLine();
+            value = value.trim();
+
+            if (value.length() < 1) {
+                System.out.println("\nInvaild value: value can not be blank");
+                continue;
+            }
+
+            break;
+        }
+        return value;
+    }
+
+    private boolean doAction(String fuelPercentageOption) {
+        double userInput = Double.parseDouble(fuelPercentageOption);
+
+        this.fuelPercentage = "Surface Hardness : ";
+        String secondInput = this.getFuelPercentageOption();
+        double surfaceHardness = Double.parseDouble(secondInput);
+
+        double result = TravelControl.calcGatherFuel(userInput, surfaceHardness);
+
+        if (result <= 0) {
+            System.out.println("\n You are Okay");
+            return false;
+        } else {
+            System.out.println("\n You need Fuel");
+        }
+
+        return true;
+    }
     
 }
