fd944d051ed978995379b44530de46b3f9df63a8
Eddie Pincay <eddiepincay@gmail.com>
2017-06-07 23:10:20 -0500

    06-07-2017 FuelPercentageView added

---
+++ src/byui/cit260/moonJumpers/view/FuelPercentageView.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.moonJumpers.view;
+
+/**
+ *
+ * @author Eddie Pincay
+ */
+public class FuelPercentageView {
+    
+}
