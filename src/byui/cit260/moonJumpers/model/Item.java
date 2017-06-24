c668b1189b393bb2a999d1ad45376433c421ee73
Eddie Pincay <eddiepincay@gmail.com>
2017-06-23 23:21:09 -0500

    06-23-2017 Item Class to be an Array

--- src/byui/cit260/moonJumpers/model/Item.java
+++ src/byui/cit260/moonJumpers/model/Item.java
@@ -5,50 +5,17 @@
  */
 package byui.cit260.moonJumpers.model;
 
-import java.io.Serializable;
-
 /**
  *
  * @author Eddie Pincay
  */
-public enum Item implements Serializable {
+public enum Item {
     
-    water("It is necessary to have energy on the planet"),
-    metal("With this you can upgrade your ship."),
-    lantern("This serves to illuminate dark places"),
-    food("It is necessary to have energy"),
-    fuel("Necesario para nuestra nave y para generar fuego."),
-    spacecraft("It's necessary to travel between the planets.");
+    water,
+    metal,
+    lantern,
+    food,
+    fuel,
+    spacecraft;
     
-    private final String description;
-    private final Double quantityInStock;
-    private final Double requiredAmount;
-
-    Item(String description) {
-        this.description = description;
-        quantityInStock = new Double("5");
-        requiredAmount = new Double("2");
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getQuantityInStock() {
-        return quantityInStock;
-    }
-
-    public double getRequiredAmount() {
-        return requiredAmount;
-    }
-
-    @Override
-    public String toString() {
-        return "Items{" + "description=" + description + ", quantityInStock=" + quantityInStock + ", requiredAmount=" + requiredAmount + '}';
-    }
-
-  
-
- 
- 
 }
