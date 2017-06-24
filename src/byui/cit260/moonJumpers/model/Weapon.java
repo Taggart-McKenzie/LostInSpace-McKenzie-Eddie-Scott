63b15a346be39a2574b6c0b05804f692289478f8
Eddie Pincay <eddiepincay@gmail.com>
2017-06-23 20:57:19 -0500

    06-23-2017 Weapon Class to be a Weapon Enum

--- src/byui/cit260/moonJumpers/model/Weapon.java
+++ src/byui/cit260/moonJumpers/model/Weapon.java
@@ -6,88 +6,40 @@
 package byui.cit260.moonJumpers.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  *
  * @author mckenzietaggart
  */
-public class Weapon implements Serializable {
+public enum Weapon implements Serializable {
     
-    //class instance variables
-    private String name;
-    private String description;
-    private double strength;
+    Stir("You have 3 shots to use them. Less 20 points"),
+    MagneticWeapon("It is damaged after 5 attempts. Less 15 points"),
+    HealingSolution("You have a chance to heal 50% of the total life and you have 2 times."),
+    Knife("You can lower only 10 points per attack."),
+    HomePump("Destroy a question");
     
-    //default constructor
-    public Weapon() {
+    private final String description;
+    private final double strength;
+    
+    Weapon(String description) {
+        this.description = description;
+        strength = new Double("10");
     }
     
-    
-    //getters and setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public double getStrength() {
         return strength;
     }
 
-    public void setStrength(double strength) {
-        this.strength = strength;
-    }
-    
-    //equals and hashCode functions
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 31 * hash + Objects.hashCode(this.name);
-        hash = 31 * hash + Objects.hashCode(this.description);
-        hash = 31 * hash + (int) (Double.doubleToLongBits(this.strength) ^ (Double.doubleToLongBits(this.strength) >>> 32));
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Weapon other = (Weapon) obj;
-        if (Double.doubleToLongBits(this.strength) != Double.doubleToLongBits(other.strength)) {
-            return false;
-        }
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (!Objects.equals(this.description, other.description)) {
-            return false;
-        }
-        return true;
-    }
-    
     //toString function
 
     @Override
     public String toString() {
-        return "Weapon{" + "name=" + name + ", description=" + description + ", strength=" + strength + '}';
+        return "Weapon{" + "description=" + description + ", strength=" + strength + '}';
     }
     
 }
