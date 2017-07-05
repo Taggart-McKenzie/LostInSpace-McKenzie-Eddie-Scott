cf91c13496093e207430ff5718f21af61471c5a5
McKenzie <tag16004@byui.edu>
2017-07-05 09:28:42 -0700

    7/5 - group project to page 9

---
+++ src/byui/cit260/moonJumpers/view/ErrorView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.moonJumpers.view;
+
+import java.io.PrintWriter;
+import moonjumpers.MoonJumpers;
+
+/**
+ *
+ * @author mckenzietaggart
+ */
+public class ErrorView {
+    
+    private static final PrintWriter errorFile = MoonJumpers.getOutFile();
+    
+    public static void display (String className, String errorMessage) {
+        errorFile.println(
+                "-------------------------------------------------------"
+                + "\n- ERROR - " + errorMessage
+                + "\n---------------------------------------------------");      
+    }
+    
+}
--- src/moonjumpers/MoonJumpers.java
+++ src/moonjumpers/MoonJumpers.java
@@ -15,6 +15,12 @@ import byui.cit260.moonJumpers.model.Actor;
 import byui.cit260.moonJumpers.model.Game;
 import byui.cit260.moonJumpers.model.Weapon;
 import byui.cit260.moonJumpers.view.StartProgramView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,6 +29,53 @@ import byui.cit260.moonJumpers.view.StartProgramView;
 public class MoonJumpers {
 
     private static Game currentGame = null;
+    private static Player player = null;
+
+    private static PrintWriter outFile = null;
+    private static BufferedReader inFile = null;
+    
+    private static PrintWriter logFile = null;
+
+    
+    public static void main(String[] args) {
+
+        StartProgramView startProgramView = new StartProgramView();
+        try {
+            //open character stream files for end user input and output
+            MoonJumpers.inFile = 
+                    new BufferedReader(new InputStreamReader(System.in));
+            MoonJumpers.outFile = new PrintWriter(System.out, true);
+            
+            //open log file
+            String filePath = "log.txt";
+            MoonJumpers.logFile = new PrintWriter(filePath);
+            
+            //create StartProgramView and start the program
+            startProgramView.display();
+            
+        } catch (Throwable te) {
+            System.out.println("Exception: " + te.toString() +
+                               "\nCause: " + te.getCause() +
+                               "\nMessage: " + te.getMessage());
+            te.printStackTrace();
+            //System.out.println(te.getMessage());
+            //te.printStackTrace();
+            //startProgramView.display();
+        }
+        finally {
+            try {
+                if (MoonJumpers.inFile != null)
+                    MoonJumpers.inFile.close();
+                if (MoonJumpers.outFile != null)
+                    MoonJumpers.outFile.close();
+            } catch (IOException ex) {
+                System.out.println("Error closing files");
+                return;
+            }
+
+        }
+        
+    }
 
     public static Game getCurrentGame() {
         return currentGame;
@@ -31,9 +84,7 @@ public class MoonJumpers {
     public static void setCurrentGame(Game currentGame) {
         MoonJumpers.currentGame = currentGame;
     }
-
-    private static Player player = null;
-
+    
     public static Player getPlayer() {
         return player;
     }
@@ -41,23 +92,28 @@ public class MoonJumpers {
     public static void setPlayer(Player player) {
         MoonJumpers.player = player;
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-
-        StartProgramView startProgramView = new StartProgramView();
-        try {
-            //create StartProgramView and start the program
-            startProgramView.display();
-        } catch (Throwable te) {
-            System.out.println(te.getMessage());
-            te.printStackTrace();
-            startProgramView.display();
-        }
-   
-        
+    
+    public static PrintWriter getOutFile() {
+        return outFile;
     }
 
+    public static void setOutFile(PrintWriter outFile) {
+        MoonJumpers.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        MoonJumpers.inFile = inFile;
+    }
+
+    public static PrintWriter getLogFile() {
+        return logFile;
+    }
+
+    public static void setLogFile(PrintWriter logFile) {
+        MoonJumpers.logFile = logFile;
+    }
 }
